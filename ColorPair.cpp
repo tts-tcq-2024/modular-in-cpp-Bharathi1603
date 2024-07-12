66006dd2d0a870e0505a317d0b1bfe426dd28207
Bharathi1603 <bharathikec013@gmail.com>
2024-07-12 17:25:07 +0530

    Create ColorPair.cpp

---
+++ ColorPair.cpp
@@ -0,0 +1,15 @@
+#include "ColorPair.h"
+
+ColorPair::ColorPair(const MajorColorHandler::Name majorColor, const MinorColorHandler::Name minorColor)
+    : majorColor(majorColor), minorColor(minorColor) {}
+
+MajorColorHandler::Name ColorPair::getMajorColor() const { return majorColor; }
+
+MinorColorHandler::Name ColorPair::getMinorColor() const { return minorColor; }
+
+std::string ColorPair::toString() const {
+  std::string colorPairStr = MajorColorHandler::getMajorColorName(majorColor);
+  colorPairStr += " ";
+  colorPairStr += MinorColorHandler::getMinorColorName(minorColor);
+  return colorPairStr;
+}
