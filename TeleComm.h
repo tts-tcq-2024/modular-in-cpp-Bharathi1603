49e9a77af0be1b8563379862d7b1233c78dae295
Bharathi1603 <bharathikec013@gmail.com>
2024-07-13 11:40:31 +0530

    Update TeleComm.h

--- TeleComm.h
+++ TeleComm.h
@@ -1,9 +1,13 @@
 #pragma once
 #include "ColorPair.h"
+#include <vector>
 
 class TeleComm
 {
+  private:
+    static const maxColorPair = 25;
   public:
     static ColorPair getColorFromPairNumber(int pairNumber);
     static int getPairNumberFromColor(MajorColorHandler::Name major, MinorColorHandler::Name minor);
+    static std::vector<ColorPair> getColorPairList(int pairNumber);
 };
