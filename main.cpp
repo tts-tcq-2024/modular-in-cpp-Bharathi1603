71e651a8d73003962932b9fce19e7002b771781c
Bharathi1603 <bharathikec013@gmail.com>
2024-07-13 10:47:14 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,17 +2,6 @@
 #include <assert.h>
 #include "TeleComm.h"
 
-void testNumberToPair(int pairNumber,
-    TelCoColorCoder::MajorColor expectedMajor,
-    TelCoColorCoder::MinorColor expectedMinor)
-{
-    TelCoColorCoder::ColorPair colorPair =
-        TelCoColorCoder::GetColorFromPairNumber(pairNumber);
-    std::cout << "Got pair " << colorPair.ToString() << std::endl;
-    assert(colorPair.getMajor() == expectedMajor);
-    assert(colorPair.getMinor() == expectedMinor);
-}
-
 void testNumberToPair(int pairNumber, MajorColorHandle::Name expectedMajor, MinorColorHandle::Name expectedMinor)
 {
     ColorPair colorPair = TeleComm::gtColorFromPairNumber(pairNumber);
